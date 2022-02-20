071042aeac5c8bc5bc0aad6ace4b735a7ab77891
dmccoystephenson <dmccoystephenson@gmail.com>
2022-02-20 23:39:37 +0000

    Prevented factionless and non-faction-owners from creating elections.

--- src/main/java/dansplugins/democracy/commands/CreateCommand.java
+++ src/main/java/dansplugins/democracy/commands/CreateCommand.java
@@ -4,7 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import dansplugins.democracy.Democracy;
 import dansplugins.democracy.factories.ElectionFactory;
+import dansplugins.factionsystem.externalapi.MF_Faction;
 import preponderous.ponder.minecraft.bukkit.abs.AbstractPluginCommand;
 
 import java.util.ArrayList;
@@ -28,6 +30,13 @@ public class CreateCommand extends AbstractPluginCommand {
             return false;
         }
         Player player = (Player) commandSender;
+
+        MF_Faction faction = Democracy.getInstance().getMedievalFactionsAPI().getFaction(player);
+        if (faction == null || !faction.getOwner().equals(player.getUniqueId())) {
+            player.sendMessage(ChatColor.RED + "You must be the owner of a faction to create an election.");
+            return false;
+        }
+
         UUID electionUUID = ElectionFactory.getInstance().createElection(player);
         if (electionUUID == null) {
             player.sendMessage(ChatColor.RED + "An election is already in progress.");
